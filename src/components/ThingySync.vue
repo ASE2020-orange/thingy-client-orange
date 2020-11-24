11bbf6aad5d4361ff94c73d5d4f851a1cf6e2ade
SergiyGolov <sergiy.goloviatinski@unine.ch>
2020-11-24 20:08:39 +0100

    thingy sync + multiplayer support

---
+++ src/components/ThingySync.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="ThingySync">
+    Thingy-{{ thingy.id }}
+    <div
+      class="list-group-item"
+      v-bind:key="index"
+      v-for="(position, index) in thingy.positions"
+      v-bind:class="isPositionSelected(index)"
+    >
+      <img height="50px" width="50px" v-bind:src="position" />
+    </div>
+    <button
+      type="button"
+      class="btn btn-primary"
+      v-on:click="selectThingy(thingy.id)"
+    >
+      Select
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ThingySync",
+  props: {
+    thingy: {
+      positions: Object,
+      id: Number,
+      positions_selected: Object,
+    },
+  },
+  methods: {
+    isPositionSelected(index) {
+      if (this.thingy.positions_selected[index]) {
+        return "selected_position bg-info";
+      } else {
+        return "";
+      }
+    },
+    selectThingy(thingy_id) {
+      this.$parent.selectThingy(thingy_id);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.selected_position img {
+  filter: invert(1);
+}
+</style>
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,57 +1,105 @@
 <template>
-<div class="Game">
-  <h2 class="mt-2" v-if="showSettings">Select the game settings</h2>
-  <b-container class="offset-sm-3" fluid id="settingsContainer" v-if="showSettings">
-    <b-row class="vh-30 text-center mt-4" align-v="center">
-      <b-col class="text-left" sm="3">
-          <label for="categorySelection">Quizz questions categories </label>
-      </b-col>
-      <b-col sm="3">
-        <select v-model ="selectedCategory" class="form-control form-control-sm" name="categorySelection" id="quizzCategoriesSelection">
-          <option v-for="category in categories" v-bind:value="{id: category.id, name: category.name}" :key="category.id">
-            {{category.name}}
-          </option>
-        </select>
-      </b-col>
-    </b-row>
-    <b-row class="vh-30 text-left mt-2" align-v="center">
-      <b-col sm="3">
-          <label for="difficultySelection">Questions difficulty</label>
-      </b-col>
-      <b-col sm="3">
-        <select class="form-control form-control-sm" v-model="selectedDifficulty" name="difficultySelection" id="quizzDifficultySelection">
-          <option v-for="difficulty in difficulties" v-bind:value="{difficulty: difficulty}" :key="difficulty">
-            {{difficulty}}
-          </option>
-        </select>
-      </b-col>
-    </b-row>
-    <b-row class="vw-100 text-left mt-2">
-      <b-col sm="6">
-        <button type="button" class="btn btn-primary" v-on:click="startGame()">Start game</button>
-      </b-col>
-    </b-row>
-  </b-container>
-  
-  
-  <b-container fluid>
-  <b-row class="vh-100 text-center" align-v="center">
-    <b-col>
-        <category v-bind:category="category"></category>
-        <question v-bind:question="question"></question>
-        <b-list-group horizontal class="justify-content-center">
-          <answer v-bind:key="answer.id" v-bind:class="isAnswerSelectedClass(index)" v-bind:answer="answer" v-for="(answer, index) in answers"></answer>
-        </b-list-group>
-    </b-col>
-  </b-row>
-</b-container>
-</div>
+  <div class="Game">
+    <div v-if="thingy_id == -1">
+      You need to synchronize your Thingy in order to play the Game !
+      <br />Different players should try to position their Thingy in different
+      positions to distinguish them
+      <b-list-group horizontal class="justify-content-center">
+        <ThingySync
+          v-bind:key="index"
+          v-bind:thingy="thingy"
+          v-for="(thingy, index) in thingys"
+        ></ThingySync>
+      </b-list-group>
+    </div>
+    <div v-if="thingy_id != -1">
+      <h2 class="mt-2" v-if="showSettings">Select the game settings</h2>
+      <b-container
+        class="offset-sm-3"
+        fluid
+        id="settingsContainer"
+        v-if="showSettings"
+      >
+        <b-row class="vh-30 text-center mt-4" align-v="center">
+          <b-col class="text-left" sm="3">
+            <label for="categorySelection">Quizz questions categories </label>
+          </b-col>
+          <b-col sm="3">
+            <select
+              v-model="selectedCategory"
+              class="form-control form-control-sm"
+              name="categorySelection"
+              id="quizzCategoriesSelection"
+            >
+              <option
+                v-for="category in categories"
+                v-bind:value="{ id: category.id, name: category.name }"
+                :key="category.id"
+              >
+                {{ category.name }}
+              </option>
+            </select>
+          </b-col>
+        </b-row>
+        <b-row class="vh-30 text-left mt-2" align-v="center">
+          <b-col sm="3">
+            <label for="difficultySelection">Questions difficulty</label>
+          </b-col>
+          <b-col sm="3">
+            <select
+              class="form-control form-control-sm"
+              v-model="selectedDifficulty"
+              name="difficultySelection"
+              id="quizzDifficultySelection"
+            >
+              <option
+                v-for="difficulty in difficulties"
+                v-bind:value="{ difficulty: difficulty }"
+                :key="difficulty"
+              >
+                {{ difficulty }}
+              </option>
+            </select>
+          </b-col>
+        </b-row>
+        <b-row class="vw-100 text-left mt-2">
+          <b-col sm="6">
+            <button
+              type="button"
+              class="btn btn-primary"
+              v-on:click="startGame()"
+            >
+              Start game
+            </button>
+          </b-col>
+        </b-row>
+      </b-container>
+
+      <b-container fluid v-if="!showSettings">
+        <b-row class="vh-100 text-center" align-v="center">
+          <b-col>
+            <category v-bind:category="category"></category>
+            <question v-bind:question="question"></question>
+            <b-list-group horizontal class="justify-content-center">
+              <answer
+                v-bind:key="answer.id"
+                v-bind:class="isAnswerSelectedClass(index)"
+                v-bind:answer="answer"
+                v-for="(answer, index) in answers"
+              ></answer>
+            </b-list-group>
+          </b-col>
+        </b-row>
+      </b-container>
+    </div>
+  </div>
 </template>
 
 <script>
 import Category from "@/components/Category.vue";
 import Question from "@/components/Question.vue";
 import Answer from "@/components/Answer.vue";
+import ThingySync from "@/components/ThingySync.vue";
 
 export default {
   name: "Game",
@@ -59,197 +107,275 @@ export default {
     Category,
     Question,
     Answer,
+    ThingySync,
   },
-  created() {
-    this.getCategories();
-    //this.startGame();
+  data: () => {
+    return {
+      server_adress: "localhost:1080",
+      ws_server: undefined,
+      showSettings: true,
+
+      categories: [],
+      selectedCategory: {},
+
+      difficulties: ["Easy", "Medium", "Hard"],
+      selectedDifficulty: { difficulty: "Easy" },
+
+      positions: [
+        require("@/assets/normal.svg"),
+        require("@/assets/side.svg"),
+        require("@/assets/upside_down.svg"),
+      ],
+      position_titles: ["Normal", "On the side", "Upside down"],
+
+      question: undefined,
+      answers: new Array(),
+      category: undefined,
+      game_id: -1,
+      selected_answer: -1,
+      false_answers: new Array(),
+      thingy_id: -1,
+      thingys: new Array(),
+    };
   },
-    data: () => {
-      return {
-        server_adress: "localhost:1080",
-        ws_server: undefined,
-        showSettings: true,
+  methods: {
+    startGame() {
+      this.showSettings = false;
 
-        categories : [],
-        selectedCategory: {},
-
-        difficulties : [
-          'Easy',
-          'Medium',
-          'Hard',
-        ],
-        selectedDifficulty: {difficulty:'Easy'},
-
-
-        positions: [
-          require("@/assets/normal.svg"),
-          require("@/assets/side.svg"),
-          require("@/assets/upside_down.svg"),
-        ],
-        position_titles: ["Normal", "On the side", "Upside down"],
-
-        question: undefined,
-        answers: new Array(),
-        category: undefined,
-        game_id: -1,
-        selected_answer: -1,
-        false_answers: new Array(),
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          category: this.selectedCategory.id,
+          difficulty: this.selectedDifficulty.difficulty.toLowerCase(),
+        }),
       };
+
+      fetch(`http://${this.server_adress}/api/games/`, requestOptions)
+        .then((res) => {
+          return res.json();
+        })
+        .catch((err) => console.log(err))
+        .then((data) => {
+          this.game_id = data.game_id;
+          this.getQuestion();
+        })
+        .catch((err) => console.log(err));
     },
-    methods: {
-      startGame() {
-        this.showSettings = false;
+    isAnswerSelectedClass(index) {
+      if (index == this.selected_answer && this.false_answers.includes(index)) {
+        return "bg-info text-danger selected_wrong_answer";
+      } else if (index == this.selected_answer) {
+        return "bg-info text-light selected_answer";
+      } else if (this.false_answers.includes(index)) {
+        return "bg-danger text-light selected_answer";
+      } else {
+        return "bg-light text-black";
+      }
+    },
 
-        const requestOptions = {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ category: this.selectedCategory.id, difficulty: this.selectedDifficulty.difficulty.toLowerCase() }),
-        };
-
-        fetch(`http://${this.server_adress}/api/games/`, requestOptions)
-          .then((res) => {
-            return res.json();
-          })
-          .catch((err) => console.log(err))
-          .then((data) => {
-            this.game_id = data.game_id;
-            this.getQuestion();
-          })
-          .catch((err) => console.log(err));
-      },
-      isAnswerSelectedClass(index) {
-        if (index == this.selected_answer && this.false_answers.includes(index)) {
-          return "bg-info text-danger selected_wrong_answer";
-        } else if (index == this.selected_answer) {
-          return "bg-info text-light selected_answer";
-        } else if (this.false_answers.includes(index)) {
-          return "bg-danger text-light selected_answer";
-        } else {
-          return "bg-light text-black";
-        }
-      },
-
-      getCategories()
-      {
-          fetch(`http://${this.server_adress}/api/categories/`, {
-              method: "get",
-          })
-          .then((res) => {
-            return res.json();
-          })
-          .catch((err) => console.log(err))
-          .then((data) => {
-            this.categories = data.trivia_categories;
-            this.selectedCategory = this.categories[0];
-          })
-          .catch((err) => console.log(err));
-      },
-
-      getQuestion() {
-        fetch(
-          `http://${this.server_adress}/api/games/${this.game_id}/question/`,
-          {
-            method: "get",
-          }
-        )
-          .then((res) => {
-            return res.json();
-          })
-          .catch((err) => console.log(err))
-          .then((data) => {
-            this.answers = data.answers;
-            this.answers.forEach((x) => {
-              x.position = this.positions[this.answers.indexOf(x)];
-              x.title = this.position_titles[this.answers.indexOf(x)];
-              x.answer = this.decodeHTML(x.answer);
-            });
-            this.question = this.decodeHTML(data.question);
-            this.category = data.category;
-          })
-          .catch((err) => console.log(err));
-      },
-      showToaster(variant, title, answer_id) {
-        this.$bvToast.toast(`${this.answers[answer_id].answer}`, {
-          title: title,
-          variant: variant,
-          toaster: "b-toaster-top-center",
-          noCloseButton: true,
-        });
-      },
-      connectWS() {
-        console.log("Starting connection to WebSocket Server");
-        this.ws_server = new WebSocket(`ws://${this.server_adress}/ws`);
-
-        this.ws_server.onopen = () => {
-          this.ws_server.send("CLIENT_CONNECT");
-          console.log("Successfully connected to the websocket server...");
-        };
-
-        this.ws_server.onmessage = (event) => {
-          if (event.data.startsWith("TO_CLIENT")) {
-            let action = event.data.split(".")[1];
-            let thingy = event.data.split(".")[2];
-
-            console.log(`${action} from ${thingy}`);
-
-            switch (action) {
-              case "FLIP_A":
-                this.selected_answer = 0;
-                break;
-              case "FLIP_B":
-                this.selected_answer = 1;
-                break;
-              case "FLIP_C":
-                this.selected_answer = 2;
-                break;
-              case "BUTTON":
-                if (this.selected_answer != -1) {
-                  this.answerQuestion(this.selected_answer);
-                }
-                break;
-            }
-          }
-        };
-      },
-      decodeHTML(html) {
-        var txt = document.createElement("textarea");
-        txt.innerHTML = html;
-        return txt.value;
-      },
-      answerQuestion(answer_id) {
-        fetch(
-          `http://${this.server_adress}/api/games/${this.game_id}/question/`,
-          {
-            method: "post",
-            body: JSON.stringify({
-              answer_id: answer_id,
-            }),
-          }
-        )
-          .then((res) => {
-            return res.json();
-          })
-          .catch((err) => console.log(err))
-          .then((data) => {
-            if (data.correct) {
-              this.showToaster("success", "Correct answer", answer_id);
+    getCategories() {
+      fetch(`http://${this.server_adress}/api/categories/`, {
+        method: "get",
+      })
+        .then((res) => {
+          return res.json();
+        })
+        .catch((err) => console.log(err))
+        .then((data) => {
+          this.categories = data.trivia_categories;
+          this.selectedCategory = this.categories[0];
+        })
+        .catch((err) => console.log(err));
+    },
+    checkExistingGame() {
+      fetch(`http://${this.server_adress}/api/games/`, {
+        method: "get",
+      })
+        .then((res) => {
+          return res.json();
+        })
+        .catch((err) => console.log(err))
+        .then((data) => {
+          this.game_id = data.game_id;
+          if (this.game_id != -1) {
+            this.showSettings = false;
+            if (this.thingy_id != -1) {
               this.getQuestion();
-              this.false_answers.splice(0, this.false_answers.length);
-            } else {
-              this.showToaster("danger", "Wrong answer", answer_id);
-              this.false_answers.push(answer_id);
             }
-          })
-          .catch((err) => console.log(err));
-      },
+          }
+        })
+        .catch((err) => console.log(err));
     },
-    mounted() {
-      this.connectWS();
-      window.addEventListener("unload", () => {
-        this.ws_server.send("close");
-        this.ws_server.close();
+    getQuestion() {
+      this.$bvToast.toast("Getting next question", {
+        title: "Next question",
+        variant: "info",
+        toaster: "b-toaster-top-center",
+        noCloseButton: true,
+      });
+      fetch(
+        `http://${this.server_adress}/api/games/${this.game_id}/question/`,
+        {
+          method: "get",
+        }
+      )
+        .then((res) => {
+          return res.json();
+        })
+        .catch((err) => console.log(err))
+        .then((data) => {
+          this.answers = data.answers;
+          this.answers.forEach((x) => {
+            x.position = this.positions[this.answers.indexOf(x)];
+            x.title = this.position_titles[this.answers.indexOf(x)];
+            x.answer = this.decodeHTML(x.answer);
+          });
+          this.question = this.decodeHTML(data.question);
+          this.category = data.category;
+        })
+        .catch((err) => console.log(err));
+    },
+    showToaster(variant, title, answer_id) {
+      this.$bvToast.toast(`${this.answers[answer_id].answer}`, {
+        title: title,
+        variant: variant,
+        toaster: "b-toaster-top-center",
+        noCloseButton: true,
       });
     },
+    connectWS() {
+      console.log("Starting connection to WebSocket Server");
+      this.ws_server = new WebSocket(`ws://${this.server_adress}/ws`);
+
+      this.ws_server.onopen = () => {
+        this.ws_server.send("CLIENT_CONNECT");
+        console.log("Successfully connected to the websocket server...");
+      };
+
+      this.ws_server.onmessage = (event) => {
+        if (event.data.startsWith("TO_CLIENT")) {
+          let action = event.data.split(".")[1];
+          let thingy = event.data.split(".")[2];
+          let thingy_id = -1;
+          if (thingy != null) {
+            thingy_id = thingy.split("-")[1];
+          }
+
+          switch (action) {
+            case "FLIP_A":
+              if (this.thingy_id == thingy_id || this.thingy_id == -1) {
+                this.selected_answer = 0;
+              }
+              break;
+            case "FLIP_B":
+              if (this.thingy_id == thingy_id || this.thingy_id == -1) {
+                this.selected_answer = 1;
+              }
+
+              break;
+            case "FLIP_C":
+              if (this.thingy_id == thingy_id || this.thingy_id == -1) {
+                this.selected_answer = 2;
+              }
+              break;
+            case "BUTTON":
+              if (
+                this.thingy_id == thingy_id &&
+                this.selected_answer != -1 &&
+                this.game_id != -1
+              ) {
+                this.answerQuestion(this.selected_answer);
+              }
+              break;
+            case "GAME_FINISHED":
+              this.finishGame();
+              break;
+            case "NEXT_QUESTION":
+              this.getQuestion();
+              break;
+            case "GAME_STARTED":
+              if (this.thingy_id != -1) {
+                this.checkExistingGame();
+              }
+              break;
+          }
+
+          if (this.thingy_id == -1) {
+            for (let i = 0; i < 3; i++) {
+              this.$set(
+                this.thingys[thingy_id - 1].positions_selected,
+                i,
+                i == this.selected_answer ? true : false
+              );
+            }
+          }
+        }
+      };
+    },
+    finishGame() {
+      this.game_id = -1;
+      this.showSettings = true;
+      this.$bvToast.toast("Game finished", {
+        title: "Game finished",
+        variant: "info",
+        toaster: "b-toaster-top-center",
+        noCloseButton: true,
+      });
+    },
+    decodeHTML(html) {
+      var txt = document.createElement("textarea");
+      txt.innerHTML = html;
+      return txt.value;
+    },
+    answerQuestion(answer_id) {
+      fetch(
+        `http://${this.server_adress}/api/games/${this.game_id}/question/`,
+        {
+          method: "post",
+          body: JSON.stringify({
+            answer_id: answer_id,
+          }),
+        }
+      )
+        .then((res) => {
+          return res.json();
+        })
+        .catch((err) => console.log(err))
+        .then((data) => {
+          if (data.correct) {
+            this.showToaster("success", "Correct answer", answer_id);
+            this.false_answers.splice(0, this.false_answers.length);
+          } else {
+            this.showToaster("danger", "Wrong answer", answer_id);
+            this.false_answers.push(answer_id);
+          }
+        })
+        .catch((err) => console.log(err));
+    },
+    selectThingy(thingy_id) {
+      this.thingy_id = thingy_id;
+      this.checkExistingGame();
+    },
+  },
+  mounted() {
+    this.getCategories();
+    this.connectWS();
+    this.checkExistingGame();
+    if (this.thingy_id == -1) {
+      for (let i = 1; i < 4; i++) {
+        this.thingys.push({
+          positions: this.positions,
+          id: i,
+          positions_selected: [false, false, false],
+        });
+      }
+    }
+    window.addEventListener("unload", () => {
+      this.ws_server.send("close");
+      this.ws_server.close();
+    });
+  },
 };
 </script>
 
@@ -260,7 +386,8 @@ export default {
 }
 
 .selected_wrong_answer img {
-  filter: invert(13%) sepia(83%) saturate(7496%) hue-rotate(1deg) brightness(93%) contrast(120%);
+  filter: invert(13%) sepia(83%) saturate(7496%) hue-rotate(1deg)
+    brightness(93%) contrast(120%);
 }
 
 .fluid-container.footer {
